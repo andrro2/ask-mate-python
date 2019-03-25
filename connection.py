a6ba41ac15ba458e9fb1e8805beb825e9853a7a1
András Rozner <roznernek@gmail.com>
2019-03-25 14:57:44 +0100

    list.html and server updated to work with the data

--- connection.py
+++ connection.py
@@ -1,38 +1,23 @@
 import csv
 
+filename = '/home/rozner/codecool/web/1st_tw/ask-mate-python/sample_data/question.csv'
 DATA_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
 
 def get_user_story(story_id=None):
     return get_csv_data(story_id)
 
 def get_csv_data(filename, one_user_story_id=None):
-    """
-    :param one_user_story_id:
-        If given, it will act as a filter and return the dictionary of one specific User Story
-        If not given, it will return a list of dictionaries with all the details
-    :return:
-    """
-    #  create a temporary list to read each line
     user_questions = []
 
-    #  open csv file to read
     with open(filename, encoding='utf-8') as csvfile:
-        #  use DictReader to directly create dictionaries from each lines in the csv file
         reader = csv.DictReader(csvfile)
 
-        #  read all lines in csv file
         for row in reader:
-            #  make a copy of the read row, since we can't modify it
             user_story = dict(row)
 
-            # if filtered, then just return this _found_ user story
             if one_user_story_id is not None and one_user_story_id == user_story['id']:
                 return user_story
 
-            #  store modified data in temporary list
             user_questions.append(user_story)
-    print(user_questions)
-    # return the temporary list
     return user_questions
 
-get_csv_data('/home/zsana/Codecool/WEB/ask-mate-python/sample_data/question.csv')
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import connection
 
 app = Flask(__name__)
-
+filename = '/home/rozner/codecool/web/1st_tw/ask-mate-python/sample_data/question.csv'
 
 @app.route('/')
 @app.route('/list')
 def list_route():
-    questions = connection.get_user_story(url_for('question.csv'))
+    questions = connection.get_user_story(filename)
     return render_template('list.html', questions=questions)
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=8000,

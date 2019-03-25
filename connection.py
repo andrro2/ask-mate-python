31d2171f560643d69301b65ff5db3e042bcdca20
Zsuzsanna Erdos <ezsana@gmail.com>
2019-03-25 14:49:33 +0100

    Add data_file_path

--- connection.py
+++ connection.py
@@ -1,11 +1,13 @@
 import csv
 
+DATA_FILE_PATH_QUESTION = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
+DATA_FILE_PATH_ANSWER = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
 DATA_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
 
 def get_user_story(story_id=None):
     return get_csv_data(story_id)
 
-def get_csv_data(filename, one_user_story_id=None):
+def get_csv_data(one_user_story_id=None):
     """
     :param one_user_story_id:
         If given, it will act as a filter and return the dictionary of one specific User Story
@@ -16,7 +18,7 @@ def get_csv_data(filename, one_user_story_id=None):
     user_questions = []
 
     #  open csv file to read
-    with open(filename, encoding='utf-8') as csvfile:
+    with open(DATA_FILE_PATH_QUESTION encoding='utf-8') as csvfile:
         #  use DictReader to directly create dictionaries from each lines in the csv file
         reader = csv.DictReader(csvfile)
 
@@ -34,5 +36,3 @@ def get_csv_data(filename, one_user_story_id=None):
     print(user_questions)
     # return the temporary list
     return user_questions
-
-get_csv_data('/home/zsana/Codecool/WEB/ask-mate-python/sample_data/question.csv')

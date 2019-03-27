b1a1037ddd75a8102ee76af0026772fa3ec804ea
András Rozner <roznernek@gmail.com>
2019-03-27 11:22:51 +0100

    basics finished

---
+++ data_manager.py
@@ -0,0 +1,91 @@
+import csv
+import time
+import os
+
+
+
+script_dir = os.path.dirname(__file__)
+question_rel_path = "sample_data/question.csv"
+QUESTION_FILEPATH = os.path.join(script_dir, question_rel_path)
+answer_rel_path = "sample_data/answer.csv"
+ANSWER_FILEPATH = os.path.join(script_dir, answer_rel_path)
+
+
+def get_question_data(question_id=None):
+    user_questions = []
+
+    with open(QUESTION_FILEPATH, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            question = dict(row)
+
+            if question_id is not None and question_id == question['id']:
+                return question
+
+            user_questions.append(question)
+    return user_questions
+
+
+def get_answer_data(question_id=None):
+    question_answers = []
+
+    with open(ANSWER_FILEPATH, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            answer = dict(row)
+
+            if question_id is not None and question_id == answer['question_id']:
+                question_answers.append(answer)
+
+    return question_answers
+
+
+def get_whole_answer_data():
+    answers = []
+    with open(ANSWER_FILEPATH, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            answer = dict(row)
+            answers.append(answer)
+    print(answers)
+    return answers
+
+
+def generate_new_id(existing_data):
+    new_id = 0
+    for item in existing_data:
+        if int(item['id']) >= new_id:
+            new_id = int(item['id']) + 1
+    return new_id
+
+
+def update_question_data(title, message, image):
+    existing_data = get_question_data()
+    id = generate_new_id(existing_data)
+    submission_time = time.time()
+    wiew_number = 0
+    vote_number = 0
+    dict = {'id':id, 'submission_time': submission_time, 'view_number': wiew_number, 'vote_number':vote_number, 'title':title, 'message':message, 'image':image}
+    existing_data.append(dict)
+    return existing_data
+
+
+def update_answer_data(message, image, question_id):
+    existing_data = get_whole_answer_data()
+    id = generate_new_id(existing_data)
+    submission_time = time.time()
+    vote_number = 0
+    dict = {'id': id, 'submission_time': submission_time, 'vote_number': vote_number, 'question_id': question_id, 'message': message, 'image': image}
+    existing_data.append(dict)
+    return existing_data
+
+
+def write_data(filepath, fieldnames, data):
+    with open(filepath, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
--- server.py
+++ server.py
@@ -1,22 +1,60 @@
 from flask import Flask, render_template, redirect, request, url_for
-import connection
+import data_manager
+import time
 import os
 
+QUESTION_DATA_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
+ANSWWER_DATA_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+script_dir = os.path.dirname(__file__)
+question_rel_path = "sample_data/question.csv"
+QUESTION_FILEPATH = os.path.join(script_dir, question_rel_path)
+answer_rel_path = "sample_data/answer.csv"
+ANSWER_FILEPATH = os.path.join(script_dir, answer_rel_path)
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/list')
 def list_route():
-    questions = connection.get_question_data()
+    questions = sorted(data_manager.get_question_data(), key=lambda key: key['submission_time'], reverse=True)
     return render_template('list.html', questions=questions)
 
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def route_selected_question(question_id: int):
-    question = connection.get_question_data(question_id)
-    answers = connection.get_answer_data(question_id)
-    return render_template('question.html', question=question, answers=answers)
+    question = data_manager.get_question_data(question_id)
+    question['submission_time'] = time.ctime(float(question['submission_time']))
+    answers = data_manager.get_answer_data(question_id)
+    for item in answers:
+        item['submission_time'] = time.ctime(float(item['submission_time']))
+    return render_template('question.html', question=question, answers=answers, question_id=question_id)
+
+
+@app.route('/add_question', methods=['GET', 'POST'])
+def add_question():
+    if request.method == 'POST':
+        message = request.form.get("message")
+        title = request.form.get('title')
+        image = request.form.get('image')
+        data = data_manager.update_question_data(title, message, image)
+        question_id = data[-1]['id']
+        filepath = QUESTION_FILEPATH
+        data_manager.write_data(filepath, QUESTION_DATA_HEADER, data)
+        return redirect(f'/question/{question_id}')
+    return render_template('add_question.html')
+
+
+@app.route('/question/<question_id>/add_answer', methods = ['GET', 'POST'])
+def add_answer(question_id: int):
+    if request.method == 'POST':
+        message = request.form.get('message')
+        picture = request.form.get('picture')
+        data = data_manager.update_answer_data(message, picture, question_id)
+        filepath = ANSWER_FILEPATH
+        data_manager.write_data(filepath, ANSWWER_DATA_HEADER, data)
+        return redirect(f'/question/{question_id}')
+
+    return render_template('add_answer.html', question_id=question_id)
 
 if __name__ == '__main__':
     app.run(

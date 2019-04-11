782c664babf2b3ab42a57d4269eb56953b792395
Nees612 <vekker15@gmaill.com>
2019-04-11 16:16:38 +0200

    Added css , looks nice

--- server.py
+++ server.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def latest_questions():
     questions = data_manager.get_latest_five_questions()
-    return render_template('list.html', questions=questions)
+    return render_template('main.html', questions=questions)
+
 
 @app.route('/list')
 def list_route():
     questions = sorted(data_manager.get_question_data(), key=lambda key: key['submission_time'], reverse=True)
-    return render_template('list.html', questions=questions)
+    return render_template('all_questions.html', questions=questions)
 
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
@@ -25,7 +26,7 @@ def route_selected_question(question_id: int):
     answer_comments = {}
     for a_id in answer_ids:
         for i in a_id:
-            answer_comments[a_id[i]] = (data_manager.get_answer_comment(a_id[i]))
+            answer_comments[a_id[i]] = data_manager.get_answer_comment(a_id[i])
     return render_template('question.html', question=question, question_id=question_id, answers=answers,
                            question_comments=question_comments, answer_comments=answer_comments)
 
@@ -39,7 +40,8 @@ def add_question():
         view_number = 0
         vote_number = 0
         submission_time = datetime.now()
-        question_id = data_manager.add_question(message, title, image, view_number, vote_number, submission_time)[0]['id']
+        question_id = data_manager.add_question(message, title, image, view_number, vote_number, submission_time)[0][
+            'id']
         return redirect(f'/question/{question_id}')
     return render_template('add_question.html')
 
@@ -76,24 +78,24 @@ def add_question_comment(question_id: int):
         now_time = datetime.now()
         data_manager.add_comment_to_question(question_id, user_comment, now_time)
         return redirect(f'/question/{question_id}')
-    return render_template('add_comment.html')
+    return render_template('add_comment_question.html', question_id=question_id)
 
 
-@app.route('/answer/<answer_id>/add_comment', methods=['GET', 'POST'])
-def add_answer_comment(answer_id: int):
+@app.route('/answer/<answer_id><question_id>/add_comment', methods=['GET', 'POST'])
+def add_answer_comment(answer_id: int, question_id: int):
     if request.method == 'POST':
         user_comment = request.form.get('comment')
         now_time = datetime.now()
         data_manager.add_comment_to_answer(answer_id, user_comment, now_time)
-        return redirect('/')
-    return render_template('add_comment.html')
+        return redirect(f'/question/{question_id}')
+    return render_template('add_comment_answer.html', answer_id=answer_id, question_id=question_id)
 
 
 @app.route('/search')
 def search():
-    search = '%' + request.args.get('search') +'%'
+    search = '%' + request.args.get('search') + '%'
     questions = data_manager.search(search)
-    return render_template('list.html', questions=questions, search=search)
+    return render_template('main.html', questions=questions, search=search)
 
 
 @app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
